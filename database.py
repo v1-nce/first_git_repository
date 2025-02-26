114623670be99f446ddf56994b133b4b44e6eeb1
v1-nce <vincentongkaisen6@gmail.com>
2025-02-26 21:11:57 +0800

    Cleaned up repo

---
+++ database.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, x):
+        new_node = Node(x) 
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
---
+++ first_git_file copy.py
@@ -0,0 +1,15 @@
+from database import LinkedList
+
+lst = LinkedList()
+lst.append(1)
+lst.append(2)
+lst.append(3)
+lst.display()
+
+lst.append(4)
+lst.append(5)
+lst.display()
+
+
+
+
